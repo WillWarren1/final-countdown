09fb58f66f0875a651430beaa38899eb807af239
Will Warren <willwarrenmail@gmail.com>
2019-02-15 10:11:34 -0500

    worked on css

--- public/main.js
+++ public/main.js
@@ -1,6 +1,7 @@
 const main = () => {
   if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+    document.querySelector('h1.hello-world').textContent =
+      'Explore your universe.'
   }
 }
 
