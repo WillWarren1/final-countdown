0f9b10bf33735988c71ecb401b1bdadb9d2004d4
Will Warren <willwarrenmail@gmail.com>
2019-02-15 12:51:56 -0500

    got hero image

--- public/main.js
+++ public/main.js
@@ -3,6 +3,21 @@ const main = () => {
     document.querySelector('h1.hello-world').textContent =
       'Explore your universe.'
   }
+  getNasaImage()
+}
+
+const getNasaImage = () => {
+  fetch('https://sdg-astro-api.herokuapp.com/api/Nasa/apod')
+    .then(resp => {
+      return resp.json()
+    })
+    .then(pic => {
+      console.log(pic)
+      let hero = pic.url
+      document
+        .querySelector('.hero-image')
+        .setAttribute('style', `background-image: url("${hero}")`)
+    })
 }
 
 document.addEventListener('DOMContentLoaded', main)
