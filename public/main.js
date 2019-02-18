64a1e2d36c2cb95bae4f6e937c9fc55c2a1ae6b8
Will Warren <willwarrenmail@gmail.com>
2019-02-18 17:30:52 -0500

    calculated time and converted to/from unix

--- public/main.js
+++ public/main.js
@@ -28,4 +28,28 @@ const getNasaImage = () => {
     })
 }
 
+let time = new Date(1551512700 * 1000)
+console.log(time)
+let today = new Date()
+let dd = today.getDate()
+let mm = today.getMonth() + 1
+let yyyy = today.getFullYear()
+let hour = today.getHours()
+let minute = today.getMinutes()
+let seconds = today.getSeconds()
+if (dd < 10) {
+  dd = '0' + dd
+}
+if (mm < 10) {
+  mm = '0' + mm
+}
+today = mm + '.' + dd + '.' + yyyy + '.' + hour + '.' + minute + '.' + seconds
+console.log(today)
+
+let todayUnix =
+  new Date(Date.UTC(yyyy, mm, dd, hour, minute, seconds)).getTime() / 1000
+
+console.log(todayUnix)
+
+console.log(new Date(todayUnix * 1000))
 document.addEventListener('DOMContentLoaded', main)
