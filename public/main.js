abad6442b7016f4d859685180dae85f48814c613
Will Warren <willwarrenmail@gmail.com>
2019-02-18 21:14:23 -0500

    attempting conversion for countdown

--- public/main.js
+++ public/main.js
@@ -39,18 +39,18 @@ let mm = today.getMonth()
 let yyyy = today.getFullYear()
 let hour = today.getHours()
 let minute = today.getMinutes()
-let seconds = today.getSeconds()
+let second = today.getSeconds()
 if (dd < 10) {
   dd = '0' + dd
 }
 if (mm < 10) {
   mm = '0' + mm
 }
-today = mm + '.' + dd + '.' + yyyy + '.' + hour + '.' + minute + '.' + seconds
+today = mm + '.' + dd + '.' + yyyy + '.' + hour + '.' + minute + '.' + second
 console.log(today)
 
 let todayUnix =
-  new Date(Date.UTC(yyyy, mm, dd, hour, minute, seconds)).getTime() / 1000
+  new Date(Date.UTC(yyyy, mm, dd, hour, minute, second)).getTime() / 1000
 
 console.log(todayUnix)
 console.log(new Date(todayUnix * 1000))
@@ -64,12 +64,29 @@ const getLaunchData = () => {
       console.log(launchData)
       let detail = launchData[0].details
       let launchSite = launchData[0].launch_site.site_name_long
+      let launchTime = launchData[0].launch_date_unix
+      let timeDifference = Math.abs(launchTime - todayUnix) / 1000
+      //days
+      let days = Math.floor(timeDifference / 86400)
+      timeDifference -= days * 86400
+      //hours
+      let hours = Math.floor(timeDifference / 3600) % 24
+      timeDifference -= hours * 3600
+      //minutes
+      let minutes = Math.floor(timeDifference / 60) % 60
+      timeDifference -= minutes * 60
+      //seconds
+      let seconds = Math.floor(timeDifference % 60)
       console.log(detail)
       console.log(launchSite)
+      if (detail == null) {
+        detail = 'No Description Available yet.'
+      }
+      document.querySelector('.details').textContent = detail
+      document.querySelector('.launch-time').textContent =
+        days + ' days, ' + minutes + ' mins, ' + seconds + 'seconds'
+      document.querySelector('.launch-site').textContent = launchSite
     })
-  if ((detail = null)) {
-    detail = 'No Description Available yet.'
-  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
